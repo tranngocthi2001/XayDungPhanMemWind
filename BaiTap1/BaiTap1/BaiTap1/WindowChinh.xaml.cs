4336cf6dd7755b6c22ad409e5b649504841e9c17
thi <thi.dh51901412@gmail.com>
2024-03-01 05:02:04 +0700

    addBaiTap1

---
+++ BaiTap1/BaiTap1/BaiTap1/WindowChinh.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BaiTap1
+{
+    /// <summary>
+    /// Interaction logic for WindowChinh.xaml
+    /// </summary>
+    public partial class WindowChinh : Window
+    {
+        public WindowChinh()
+        {
+            InitializeComponent();
+        }
+        private void menuMonhoc_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow f = new MainWindow();
+            f.Show();
+        }
+        private void menuHocvien_Click(object sender, RoutedEventArgs e)
+        {
+            WindowHocvien f=new WindowHocvien();
+            f.Show();
+        }
+    }
+}
